7633f1fc234539c8b3da806758ac258e3544423f
MartianTribe <steve@martiantribe.com>
2018-12-06 13:22:10 -0500

    Working on side nav collapse and h

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function( $ ) {
-
+	
+    var collapseItem = localStorage.getItem('collapseItem'); 
+    if (collapseItem) {
+       $(collapseItem).collapse('show')
+    }
+	
   // Smooth scroll for the menu and links with .scrollto classes
   $('.smoothscroll').on('click', function(e) {
     e.preventDefault();
@@ -13,8 +18,30 @@ jQuery(document).ready(function( $ ) {
       }
     }
   });
-
+  
+  //toggle side nav
   $(".navbar-collapse a").on('click', function() {
     $(".navbar-collapse.collapse").removeClass('in');
   });
+  
+  $('a.pb-menu-click').click(function(event) {
+	  	
+/*
+	  	//close any open menus
+	  	var arrOpenedDivs= document.querySelectorAll(".show");
+	  	for (i = 0; i < arrOpenedDivs.length; i++) {
+	  		arrOpenedDivs[i].classList.remove('show');
+		}	
+*/
+	
+	  
+       let selectedMenu = $(this).data('prebidclickid');
+       let thisMenuDiv = document.getElementById(selectedMenu);
+       $(thisMenuDiv).addClass('show')
+       
+       localStorage.setItem('collapseItem', $(thisMenuDiv));
+       
+  });
+
+ 
 });
