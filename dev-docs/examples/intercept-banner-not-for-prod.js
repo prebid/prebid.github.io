1e3f6a2afeb2226f4842f157922fcdf3b8f10d17
lpagnypubstack <lpagny@pubstack.io>
2025-04-17 12:03:16 +0200

    Basic example with debugging module instead of AppNexus placement
    Fix issue : https://github.com/prebid/prebid.github.io/issues/5614

---
+++ dev-docs/examples/intercept-banner-not-for-prod.js
@@ -0,0 +1,15 @@
+pbjs.que.push(() => {
+    pbjs.setConfig({
+       debugging: {
+           enabled: true,
+           intercept: [{
+               when: {
+                     bidder: 'appnexus',
+               },
+               then: {
+                     ad: '<html><body><img src="https://vcdn.adnxs.com/p/creative-image/27/c0/52/67/27c05267-5a6d-4874-834e-18e218493c32.png" width="300" height="250" /></body></html>'
+               }
+           }]
+       }
+    })
+ })
