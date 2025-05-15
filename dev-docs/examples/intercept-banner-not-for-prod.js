be9a01d1354b330240094960d357427f292ad1c1
Muki Seiler <muuki88@users.noreply.github.com>
2025-05-15 15:56:36 +0200

    Update dev-docs/examples/intercept-banner-not-for-prod.js

--- dev-docs/examples/intercept-banner-not-for-prod.js
+++ dev-docs/examples/intercept-banner-not-for-prod.js
@@ -8,7 +8,7 @@ pbjs.que.push(() => {
                      bidder: 'appnexus',
                },
                then: {
-                     ad: '<html><body><img src="https://vcdn.adnxs.com/p/creative-image/27/c0/52/67/27c05267-5a6d-4874-834e-18e218493c32.png" width="300" height="250" /></body></html>'
+                     ad: '<html><body><img src="https://files.prebid.org/creatives/prebid300x250.png" width="300" height="250" /></body></html>'
                }
            }]
        }
